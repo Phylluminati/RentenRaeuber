1e803412c3c8e813e7bddd22c85da45545fe8bd4
Phylluminati <149372006+Phylluminati@users.noreply.github.com>
2025-07-10 00:04:37 +0200

    0.0.4 Follower NPCs and more Assets

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowScript : MonoBehaviour
 {
-     public float speed;
-     [SerializeField]
-    private GameObject target;
-    // Start is called before the first frame update
+    public Transform leader;
+    public float followSharpness = 0.1f;
+
+    Vector3 _followOffset;
+    [SerializeField]
+    Animator myAnimator;
+    Rigidbody myRigidbody;
+
+    Vector3 endPosition;
+
     void Start()
     {
-        
+
+        _followOffset = transform.position - leader.position;
+        myAnimator = this.GetComponentInChildren<Animator>();
+        myRigidbody = this.GetComponentInChildren<Rigidbody>();
+        print(myAnimator);
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
 
-        //Alternate Solution
-        //transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
+        Vector3 targetPosition = leader.position + _followOffset;
+
+
+        targetPosition.y = transform.position.y;
+        if (targetPosition.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += (targetPosition - transform.position) * followSharpness;
+
+
+        myAnimator.SetBool("isWalking", endPosition != targetPosition);
+
+        endPosition = targetPosition;
+
     }
+
+    
 }
